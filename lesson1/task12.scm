6c9a250f8b3834b331aa7b1642571d0c5cc9f1f5
Alexander Chupikov <spider.ch@gmail.com>
2019-06-24 00:18:55 +0700

    Fix and extend solution for task 12

--- lesson1/task12.scm
+++ lesson1/task12.scm
@@ -1,6 +1,22 @@
-(define (get-element r c)
-  (cond ((or (= c 1) (= c r) (= r 1)) 1)
-        ((or (< r 1) (< c 1) (> r c)) 0)
-        (else (+ (get-element (- r 1) (- c 1)) (get-element (- r 1) c)))
+(define (pascal_element row col)
+  (cond
+    ((or (= row col) (= col 1))
+      1
+    )
+    ; ((= row 0) 0)
+    (else
+      (+ (pascal_element (- row 1) (- col 1)) (pascal_element (- row 1) col))
+    )
   )
+)
+
+(define (pascal_iter row col)
+  (cond
+    ((= row col) (cons 1 '()))
+    (else (append (cons (pascal_element row col) '()) (pascal_iter row (+ col 1))))
+  )
+)
+
+(define (pascal_line row)
+  (pascal_iter row 1)
 )
